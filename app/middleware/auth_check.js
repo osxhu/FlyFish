89f6372214071d5f3d429834df6c6c61928b873a
june.yang <1027612662@qq.com>
2021-11-01 16:52:02 +0800

    Update: update app/middleware/auth_check.js

--- app/middleware/auth_check.js
+++ app/middleware/auth_check.js
@@ -1,9 +1,20 @@
 'use strict';
+const CODE = require('../lib/enum');
+
 module.exports = options => {
   return async function authCheck(ctx, next) {
-    // const userId = ctx.cookies.get('userId');
-    // if (!options.reqUrlWhiteList.includes(ctx.url) && !userId) return ctx.throw(403, 'NO_AUTH');
-    // ctx.userId = userId;
+    const userId = ctx.cookies.get('userId');
+    if (!options.reqUrlWhiteList.includes(ctx.url) && !userId) {
+      ctx.body = {
+        code: CODE.AUTH_FAIL,
+        msg: 'AUTH FAIL',
+        data: null,
+      };
+      ctx.status = 403;
+      return;
+    }
+
+    ctx.userId = userId;
     await next();
   };
 };
