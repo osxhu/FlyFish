a6bc8c30d03c8110337eb6a178abc479216b1217
Ethan.Du <ethan.dwq@foxmail.com>
2021-11-13 17:33:35 +0800

    add some params

--- app/service/component.js
+++ app/service/component.js
@@ -22,10 +22,13 @@ class ComponentService extends Service {
   async getList(requestData) {
     const { ctx } = this;
 
-    const { key, isLib, name, projectId, developStatus, type, category, subCategory, curPage, pageSize } = requestData;
+    const { key, name, isLib, tags, trades, projectId, developStatus, type, category, subCategory, curPage, pageSize } = requestData;
     const queryCond = {
       status: Enum.COMMON_STATUS.VALID,
     };
+    const queryProjects = [];
+    if (projectId) queryProjects.push(projectId);
+
     queryCond.$or = [];
     if (key) {
       queryCond.$or.push({ name: { $regex: key } });
@@ -38,33 +41,42 @@ class ComponentService extends Service {
     }
     if (developStatus) queryCond.developStatus = developStatus;
     if (type) queryCond.type = type;
-    if (projectId) queryCond.projects = projectId;
     if (_.isBoolean(isLib)) queryCond.isLib = isLib;
+    if (!_.isEmpty(tags)) queryCond.tags = { $in: tags };
 
     const users = await ctx.model.User._find();
-    const projects = await ctx.model.Project._find();
-    const tags = await ctx.model.Tag._find();
+    const projectList = await ctx.model.Project._find();
+    const tagList = await ctx.model.Tag._find();
 
     const matchUsers = (users || []).filter(user => user.username.includes(key));
-    const matchProjects = (projects || []).filter(project => project.name.includes(key));
-    const matchTags = (tags || []).filter(tag => tag.name.includes(key));
+    const matchProjects = (projectList || []).filter(project => project.name.includes(key));
+    const matchTags = (tagList || []).filter(tag => tag.name.includes(key));
 
     const matchUserIds = matchUsers.map(user => user.id);
     const matchProjectIds = matchProjects.map(project => project.id);
     const matchTagIds = matchTags.map(tag => tag.id);
 
+    if (!_.isEmpty(trades)) {
+      const tradeProjects = (projectList || []).filter(project => {
+        const matchTrade = (trades || []).find(trade => (project.trades || []).includes(trade));
+        return !_.isEmpty(matchTrade);
+      });
+      const tradeProjectIds = (tradeProjects || []).map(project => project.id);
+      if (!_.isEmpty(tradeProjectIds)) queryProjects.push(...tradeProjectIds);
+    }
+    if (!_.isEmpty(queryProjects)) queryCond.projects = { $in: queryProjects };
+
     if (!_.isEmpty(matchUserIds)) queryCond.$or.push({ creator: { $in: matchUserIds } });
     if (!_.isEmpty(matchProjectIds)) queryCond.$or.push({ projects: { $in: matchProjectIds } });
     if (!_.isEmpty(matchTagIds)) queryCond.$or.push({ tags: { $in: matchTagIds } });
-
     if (_.isEmpty(queryCond.$or)) delete queryCond.$or;
     const componentList = await ctx.model.Component._find(queryCond);
 
     const total = componentList.length || 0;
     const data = (componentList || []).slice(curPage, curPage + pageSize).map(component => {
       const curUser = (users || []).find(user => user.id === component.creator) || {};
-      const curProjects = (projects || []).filter(project => (component.projects || []).includes(project.id));
-      const curTags = (tags || []).filter(tag => (component.tags || []).includes(tag.id));
+      const curProjects = (projectList || []).filter(project => (component.projects || []).includes(project.id));
+      const curTags = (tagList || []).filter(tag => (component.tags || []).includes(tag.id));
 
       return {
         id: component.id,
