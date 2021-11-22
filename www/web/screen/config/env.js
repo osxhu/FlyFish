be34b8b704a89f81bf96e7a9e7b4def8c2e9be36
cary.zhou <cary_zhou_web@163.com>
2021-11-22 15:03:45 +0800

    change editor.js

--- www/web/screen/config/env.js
+++ www/web/screen/config/env.js
@@ -9,15 +9,14 @@ window.DATAVI_ENV = (function () {
         debug: true,
         apiDomain,
         componentsDir: `/components`,
-        apiSuccessCode: 0, 
+        apiSuccessCode: 0,
 
         screenAPI: {
             // 大屏展示和编辑用到的API
             getScreenData: "/applications", // 获取大屏数据
             saveScreenConf: "/applications/{id}/design", // 保存大屏配置
-            uploadScreenImg: "/web/visualScreen/screenEditor/uploadScreenImg", // 上传大屏所需图片
-            deleteUploadScreenImg:
-                "/web/visualScreen/screenEditor/deleteUploadScreenImg", // 删除上传的大屏所需图片
+            uploadScreenImg: "/applications/img/{id}", // 上传大屏所需图片
+            deleteUploadScreenImg: "/applications/img/{id}", // 删除上传的大屏所需图片
             getModelList: "/web/visualScreen/screenEditor/getModelList", // 获取模型列表
             getModelData: "/web/visualScreen/screenEditor/getModelData", // 获取模型数据
             getScreenComponentList: "/applications/components/list",
