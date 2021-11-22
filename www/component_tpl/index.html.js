dd30f4b787ab5891d0f2434a4dff7dc290c913a9
cary.zhou <cary_zhou_web@163.com>
2021-11-22 11:00:14 +0800

    支持大屏编辑和预览

--- www/component_tpl/index.html.js
+++ www/component_tpl/index.html.js
@@ -5,7 +5,7 @@
  * @LastEditors: zhangzhiyong
  * @LastEditTime: 2021-07-27 11:39:20
  */
-'use strict';
+"use strict";
 module.exports = (componentId) => `
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -16,19 +16,18 @@ module.exports = (componentId) => `
     <link rel="stylesheet" type="text/css" href="/common/editor.css" />
 </head>
 <body>
-<div id="container" style="height: 100%;"></div>
+<div id="container"></div>
 <script type="text/javascript" src="/components/${componentId}/current/env.js"></script>
 <script type="text/javascript" src="/common/data-vi.js"></script>
 <script type="text/javascript" src="/common/editor.js"></script>
 <script type="text/javascript">
     window.onload = function () {
 
-        require(['json!./options.json','data-vi/helpers', 'datavi-editor/adapter'], function (settings,_, adapter) {
-            adapter.initComponentEditor(settings);
+        require(['json!./options.json','data-vi/helpers', 'data-vi/start'], function (settings, _, start) {
+            start.initializeBySetting(settings);
         });
     };
 </script>
 </body>
 </html>
 `;
-
