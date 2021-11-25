c7aabd5d47e5b4a1945edaf1ccb95df24869febd
freya.wang@yunzhihui.com <215866671@qq.com>
2021-11-25 11:37:46 +0800

    修改大屏跳转地址

--- web/src/public/conf/env-config.js
+++ web/src/public/conf/env-config.js
@@ -2,7 +2,7 @@ window.APP_CONFIG = (function () {
   return {
     basename: '', // 路由统一前缀，注册为微服务后必须有唯一值
     IP: '', // 后端服务IP地址
-    screen:'http://10.2.3.56:7001/web', 
+    screen:'http://10.2.3.247:7001/web', 
 
   }
 })();
