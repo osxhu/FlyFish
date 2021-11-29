90cab1a89fd976273ee86c68f749c6a7b6d115c5
june.yang <1027612662@qq.com>
2021-11-29 14:54:04 +0800

    Update: update web/screen/config/env.js

--- web/screen/config/env.js
+++ web/screen/config/env.js
@@ -2,25 +2,25 @@
  * @description 大屏配置
  */
 
-window.DATAVI_ENV = (function () {
-    const apiDomain = "http://10.2.3.56:7001";
+window.DATAVI_ENV = (function() {
+  const apiDomain = '';
 
-    return {
-        debug: true,
-        apiDomain,
-        componentsDir: `/components`,
-        apiSuccessCode: 0,
+  return {
+    debug: true,
+    apiDomain,
+    componentsDir: '/components',
+    apiSuccessCode: 0,
 
-        screenAPI: {
-            // 大屏展示和编辑用到的API
-            getScreenData: "/applications", // 获取大屏数据
-            saveScreenConf: "/applications/{id}/design", // 保存大屏配置
-            uploadScreenImg: "/applications/img/{id}", // 上传大屏所需图片
-            deleteUploadScreenImg: "/applications/img/{id}", // 删除上传的大屏所需图片
-            getModelList: "/web/visualScreen/screenEditor/getModelList", // 获取模型列表
-            getModelData: "/web/visualScreen/screenEditor/getModelData", // 获取模型数据
-            getScreenComponentList: "/applications/components/list",
-        },
-    };
+    screenAPI: {
+      // 大屏展示和编辑用到的API
+      getScreenData: '/applications', // 获取大屏数据
+      saveScreenConf: '/applications/{id}/design', // 保存大屏配置
+      uploadScreenImg: '/applications/img/{id}', // 上传大屏所需图片
+      deleteUploadScreenImg: '/applications/img/{id}', // 删除上传的大屏所需图片
+      getModelList: '/web/visualScreen/screenEditor/getModelList', // 获取模型列表
+      getModelData: '/web/visualScreen/screenEditor/getModelData', // 获取模型数据
+      getScreenComponentList: '/applications/components/list',
+    },
+  };
 }());
 
