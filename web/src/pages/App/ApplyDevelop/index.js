a6ea5d8463da6e71f6de262e38104078cf96387e
freya.wang@yunzhihui.com <215866671@qq.com>
2021-11-30 17:56:12 +0800

    fixed:修复大屏跳转地址

--- web/src/pages/App/ApplyDevelop/index.js
+++ web/src/pages/App/ApplyDevelop/index.js
@@ -248,11 +248,11 @@ const ApplyDevelop = observer(() => {
           actions={(item) => {
             return (
               <>
-                <a title="开发应用" target="_blank" href={window.APP_CONFIG.screen + '/screen/editor.html' + '?id=' + item.id} rel="noreferrer">
+                <a title="开发应用" target="_blank" href={ './web/screen/editor.html' + '?id=' + item.id} rel="noreferrer">
                   <Button value="small" type="primary">开发</Button>
 
                 </a>
-                <a title="预览应用" target="_blank" href={window.APP_CONFIG.screen + '/screen/index.html' + '?id=' + item.id} rel="noreferrer">
+                <a title="预览应用" target="_blank" href={'./web/screen/index.html' + '?id=' + item.id} rel="noreferrer">
                   <Button value="small" type="primary">预览</Button>
                 </a>
                 <Tooltip key="copy" title="复制">
